e36804473b1a8cd087d4c68f19e10588698dc24d
drnewgate <alekseibuhtin@mail.ru>
2021-01-20 22:47:49 +0300

    modal window

--- js/main.js
+++ js/main.js
@@ -28,4 +28,43 @@ $(document).ready(function() {
         disableOnInteraction: false,
       }, 
 })
+var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+      function openModal() {
+        var targetModal = $(this).attr('data-href');
+        $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
+        $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+      }
+      function closeModal(event) {
+        event.preventDefault();
+        var modalOverlay = $('.modal__overlay');
+        var modalDialog = $('.modal__dialog');
+        modalOverlay.removeClass('modal__overlay--visible')
+        modalDialog.removeClass('modal__dialog--visible')
+
+      }
+      //Обработка форм
+      $('input[type=tel]').mask('+7 (000) 000-00-00');
+      $('.form').each(function(){
+        $(this).validate({
+        errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Пожалуйста, введите имя",
+          minlength: "Имя должно быть не короче 2-х букв"
+        },
+        email: {
+          required: "Пожалуйста, введите почту",
+          email: "Формат почты должен быть name@domain.com"
+        },
+        phone: {
+          required: "Пожалуйста, введите, номер телефона ",
+          minlength:"должен содержать не менее 10 цифр"
+          },
+        },
+      });
+    });
 });
